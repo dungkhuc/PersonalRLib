1aee72896528f21255e7cb996ed10fb126e24282
Dung Khuc <khuc.quang.dung@gmail.com>
2015-01-24 19:36:27 +0200

    Functionally completed.

--- TuftePlot/R/TufteBar.R
+++ TuftePlot/R/TufteBar.R
@@ -1,21 +1,29 @@
-library(ggplot2)
-library(extrafont)
-library(ggthemes)
-tufteBar = function (dat, x, y=NULL, font="Arial", font_size= 20, col = "black") {
+require(ggplot2)
+require(extrafont)
+require(ggthemes)
+tufteBar = function (dat, x, y=NULL, color = "black",
+                     x.text.rotate = 45, y.text.rotate=0,
+                     font="Arial", font_size= 20,
+                     x.text.color = "black", y.text.color = "black") {
   # Count values in a column
-  if (is.null(y)) gplot = ggplot(dat, aes(x = order)) + geom_bar(stat="bin", width=0.6, fill=col)
+  if (is.null(y)) gplot = ggplot(dat, aes(x = order)) + geom_bar(stat="bin", width=0.6, fill=color)
   
   # Sum of values in column y, with label in column x
-  else gplot = ggplot(dat, aes_string(x = x, y = y)) + geom_bar(stat="identity", width=0.6, fill=col)
-  if (exists("gplot")) tufteTheme(gplot, font, font_size)
+  else gplot = ggplot(dat, aes_string(x = x, y = y)) + geom_bar(stat="identity", width=0.6, fill=color)
+  if (exists("gplot")) tufteTheme(gplot, x.text.rotate, y.text.rotate, font, font_size, x.text.color, y.text.color)
 }
-tufteTheme = function (tuftePlot, font, font_size) {
-  # Get ticks on y axis
+tufteTheme = function (tuftePlot, x.text.rotate, y.text.rotate, font, font_size, x.text.color, y.text.color) {
+
   tuftePlot = tuftePlot + theme_tufte(base_family=font, base_size=font_size, ticks=F)
-  #
+  
+  # Get ticks on y axis
   y.axis.ticks = ggplot_build(tuftePlot)$panel$ranges[[1]]$y.major_source
-  tuftePlot = tuftePlot + theme(axis.line=element_blank(), axis.text.x= element_text(angle = 45, hjust = 1),
-                        axis.title=element_blank()) +
-    geom_hline(yintercept=y.axis.ticks, col="white", lwd=1.2)
+  tuftePlot = tuftePlot + theme(axis.line = element_blank(),
+                                axis.text.x = element_text(angle = x.text.rotate, hjust = 1, color = x.text.color), 
+                                axis.text.y = element_text(angle=y.text.rotate, color = y.text.color),
+                                axis.title = element_blank()) +
+                          geom_hline(yintercept=y.axis.ticks, col="white", lwd=1.2)
   tuftePlot
 }
+
+
