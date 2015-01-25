92a6ece873afd8bdee602428a5f96a88edbb9c23
Dung Khuc <khuc.quang.dung@gmail.com>
2015-01-25 10:04:43 +0200

    Use palette instead of user specified color vector. Add dodge bar plot capability. Fix minor bugs.

--- TuftePlot/R/TufteBar.R
+++ TuftePlot/R/TufteBar.R
@@ -1,16 +1,31 @@
-require(ggplot2)
-require(extrafont)
-require(ggthemes)
-tufteBar = function (dat, x, y=NULL, color = "black",
-                     x.text.rotate = 45, y.text.rotate=0,
+tufteBar = function (dat, x, y=NULL, classify = NULL,
+                     width = 0.6, color = "Dark2",
+                     x.text.rotate = 0, y.text.rotate=0,
                      font="Arial", font_size= 20,
                      x.text.color = "black", y.text.color = "black") {
   # Count values in a column
-  if (is.null(y)) gplot = ggplot(dat, aes(x = order)) + geom_bar(stat="bin", width=0.6, fill=color)
+  if (is.null(y) && is.null(classify)) barplot = ggplot(dat, aes_string(x = x)) + geom_bar(stat="bin", width = width)
   
+  # Dodge identity bar plot
+  else if (!is.null(y) && !is.null(classify)) 
+    barplot = ggplot(dat, aes_string(x = x, y = y, fill = classify)) +
+      geom_bar(stat="identity", position = position_dodge(), width = width) +
+      geom_bar(stat="identity", position = position_dodge(), width = width, color = "white", show_guide = FALSE)
+  
+  # Dodge bin bar plot
+  else if (is.null(y) && !is.null(classify)) 
+    barplot = ggplot(dat, aes_string(x = x, fill = classify)) + 
+      geom_bar(stat="bin", position = position_dodge(), width = width) +
+      geom_bar(stat="bin", position = position_dodge(), width = width, color = "white", show_guide = FALSE)
   # Sum of values in column y, with label in column x
-  else gplot = ggplot(dat, aes_string(x = x, y = y)) + geom_bar(stat="identity", width=0.6, fill=color)
-  if (exists("gplot")) tufteBarTheme(gplot, x.text.rotate, y.text.rotate, font, font_size, x.text.color, y.text.color)
+  
+  # Simple identity bar plot
+  else 
+    barplot = ggplot(dat, aes_string(x = x, y = y)) + geom_bar(stat="identity", width=0.6, fill=color)
+  if (exists("barplot")) {
+    barplot =  barplot + scale_fill_brewer(palette = color)
+    tufteBarTheme(barplot, x.text.rotate, y.text.rotate, font, font_size, x.text.color, y.text.color)
+  }
 }
 tufteBarTheme = function (tuftePlot, x.text.rotate, y.text.rotate, font, font_size, x.text.color, y.text.color) {
 
@@ -20,7 +35,7 @@ tufteBarTheme = function (tuftePlot, x.text.rotate, y.text.rotate, font, font_si
   y.axis.ticks = ggplot_build(tuftePlot)$panel$ranges[[1]]$y.major_source
   tuftePlot = tuftePlot + theme(axis.line = element_blank(),
                                 axis.text.x = element_text(angle = x.text.rotate, hjust = 1, color = x.text.color), 
-                                axis.text.y = element_text(angle=y.text.rotate, color = y.text.color),
+                                axis.text.y = element_text(angle = y.text.rotate, color = y.text.color),
                                 axis.title = element_blank()) +
                           geom_hline(yintercept=y.axis.ticks, col="white", lwd=1.2)
   tuftePlot
