edb659b479c49f3e60bf8a0d4de8b12fa15cd9aa
Dung Khuc <khuc.quang.dung@gmail.com>
2015-01-25 15:26:31 +0200

    Use stat = "summary", fun.y = "sum" instead of stat = "identity". Reduce number code lines.

--- MinPlot/R/TufteBar.R
+++ MinPlot/R/TufteBar.R
@@ -1,5 +1,5 @@
 tufteBar = function (dat, x, y=NULL, classify = NULL,
-                     summary = "culmulative", y.breaks = 5,
+                     summary = "sum", y.breaks = 5,
                      width = 0.6, color = "Dark2",
                      x.text.rotate = 0, y.text.rotate=0,
                      font="Arial", font_size= 20,
@@ -8,35 +8,21 @@ tufteBar = function (dat, x, y=NULL, classify = NULL,
   if (is.null(y) && is.null(classify)) barplot = ggplot(dat, aes_string(x = x)) + geom_bar(stat="bin", width = width)
   
   # Dodge identity bar plot
-  else if (!is.null(y) && !is.null(classify)) {
-    barplot = ggplot(dat, aes_string(x = x, y = y, fill = classify, width = width))
-    if (summary == "culmulative")
-      barplot = barplot +
-        geom_bar(stat="identity", position = position_dodge(width = 0.7))
-    else if (summary == "mean") {
-      barplot = barplot + 
-        geom_bar(stat="summary", fun.y = mean, position = position_dodge()) +
-        geom_bar(stat="summary", fun.y = mean, position = position_dodge(), color = "white", show_guide=FALSE)
-    }
-  }
+  else if (!is.null(y) && !is.null(classify)) 
+    barplot = ggplot(dat, aes_string(x = x, y = y, fill = classify, width = width)) + 
+      geom_bar(stat="summary", fun.y = summary, position = position_dodge()) +
+      geom_bar(stat="summary", fun.y = summary, position = position_dodge(), color = "white", show_guide=FALSE)
   
   # Dodge bin bar plot
   else if (is.null(y) && !is.null(classify)) 
     barplot = ggplot(dat, aes_string(x = x, fill = classify)) + 
-      geom_bar(stat="bin", position = position_dodge(width = 0.7), width = width)
+      geom_bar(stat="bin", position = position_dodge(), width = width) + 
+      geom_bar(stat="bin", position = position_dodge(), width = width, color = "white", show_guide = FALSE)
   # Sum of values in column y, with label in column x
   
   # Simple identity bar plot
   else {
-    barplot = ggplot(dat, aes_string(x = x, y = y))
-    
-    # Check if user wants to display culmulative value or mean value
-    if (summary == "culmulative") {
-      barplot = barplot + geom_bar(stat="identity", width=0.6)
-    }
-    else if (summary == "mean") {
-      barplot = barplot + geom_bar(stat="summary", fun.y = mean, width=0.6)
-    }
+    barplot = ggplot(dat, aes_string(x = x, y = y, width=0.6)) + geom_bar(stat="summary", fun.y = summary)
   }
   
   if (exists("barplot")) {
